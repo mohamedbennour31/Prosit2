3c5c7a9dd1176ef436511fb144717bf4d5f9e06f
MohamedBennour13 <gepida500@gmail.com>
2024-09-18 14:55:30 +0100

    Commit

---
+++ src/Zoo.java
@@ -0,0 +1,12 @@
+public class Zoo {
+    Animal [] animals;
+    String name;
+    String city;
+    int nbrCages;
+    public Zoo( String name, String city,int nbrCages){
+        this.animals=new Animal[nbrCages];
+        this.name=name;
+        this.city=city;
+        this.nbrCages=nbrCages;
+    };
+}
---
+++ src/ZooManagement.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class ZooManagement
+{
+    //int nbrCages=20;
+    //String zooName="my zoo";
+
+    public static void main(String[] args) {
+
+       // ZooManagement zm=new ZooManagement();
+       // Scanner sc=new Scanner(System.in);
+
+        Animal lionn=new Animal();
+        Zoo zooo.new Zoo();
+        lionn.family="Carnivore";
+        lionn.name="Alex";
+        lionn.age="15";
+        lionn.isMammal=true;
+        zooo.name="Belvedere";
+        zooo.nbrCages=25;
+        zooo.city="Tunis";
+        Animal lion=new Animal("Carnivore","Alex",10,true);
+        Zoo myZoo=new Zoo("Zoo Africa", "Mourouj", 25);
+        System.out.println(lion.toString());
+        /*System.out.println("Donnez le nombre de cages");
+        while (!sc.hasNextInt()) {
+            System.out.println("Entrée invalide. Veuillez entrer un nombre entier.");
+            sc.nextInt();
+        }
+        zm.nbrCages = sc.nextInt();
+        sc.nextLine();
+
+
+        System.out.println("Donnez le nom du zoo");
+        while (true) {
+            zm.zooName = sc.nextLine();
+            if (zm.zooName.trim().isEmpty()) {
+                System.out.println("Entrée invalide. Veuillez entrer un nom valide.");
+            } else {
+                break;
+            }
+        }
+
+        System.out.println(zm.zooName + " comporte " + zm.nbrCages + " cages");
+*/
+    }
+
+}
+
